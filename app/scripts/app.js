9f09068037509fbc792ec659104d864f64996383
Katherine Mattison <kbmedisto@gmail.com>
2014-08-27 01:41:49 -0400

    come on now

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'google-maps'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -26,6 +27,18 @@ angular
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
+      })
+      .when('/scbarrierislands', {
+        templateUrl: 'views/scbarrierislands.html',
+        controller: 'MainCtrl'
+      })
+      .when('/history', {
+        templateUrl: 'views/history.html',
+        controller: 'MainCtrl'
+      })
+      .when('/edisto', {
+        templateUrl: 'views/edisto.html',
+        controller: 'MainCtrl'
       })
       .otherwise({
         redirectTo: '/'
